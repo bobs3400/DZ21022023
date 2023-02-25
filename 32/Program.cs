a4147d3285fb86fda656504b808c915b0ede6a24
bobs3400 <124318533+bobs3400@users.noreply.github.com>
2023-02-25 19:19:21 +1000

    32

---
+++ 32/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+Console.WriteLine("Введите длину массива");
+int size = int.Parse(Console.ReadLine());
+int[] Array(int size)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(-9, 9);
+    }
+    return result;
+}
+int [] Zamena(int[] Array)
+{
+    for (int i = 0; i < size; i++)
+    Array[i] = Array[i] * -1;
+    return Array;
+}
+int[] mass = Array(size);
+Console.WriteLine(String.Join(" ", mass));
+Console.WriteLine(String.Join(" ", Zamena(mass)));
