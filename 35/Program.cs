59d6a1d127c3915b5d2322fc545b59bcafb8d5fa
bobs3400 <124318533+bobs3400@users.noreply.github.com>
2023-02-28 17:30:39 +1000

    35

---
+++ 35/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+Console.WriteLine("Введите длину массива");
+int size = int.Parse(Console.ReadLine());
+int[] Array(int size)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(0, 150);
+    }
+    return result;
+}
+int Kolichestvo(int[] Array)
+{
+    int a = 0;
+    for(int i = 0; i < Array.Length; i++)
+    {
+        if(Array[i] >= 10 && Array[i] <= 99) a++;
+    }
+    return a; 
+}
+int[] mass = Array(size);
+int n = Kolichestvo(mass);
+Console.WriteLine($"В данном массиве в заданном отрезке лежит {n} элементов");
